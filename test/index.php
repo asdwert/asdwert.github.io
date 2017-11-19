2799a6f1f0bd6c7bc5850492b10617d29251fc35
Daniel Hauch <asdwert@users.noreply.github.com>
2017-11-19 12:53:52 +0100

    Add files via upload

---
+++ test/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8" />
+
+<title><Willkommen auf der Test Seite/title>
+</head>
+<body>
+<?php echo '<h1>Willkommen auf der Testseite</h1>'; ?>
+
+</body>
+</html>
